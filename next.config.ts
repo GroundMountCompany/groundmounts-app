b9613e0eaab80ff022b97cf23c6b04e831282ba6
Harshit Upadhyay <harshitupadhyay872@gmail.com>
2025-06-03 22:32:49 +0530

    headers in next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "" // Clear or override default DENY
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://groundmounts.com http://localhost:3000 http://192.168.29.68:5500"
+          }
+        ]
+      }
+    ];
+  },
   reactStrictMode: true,
   eslint: {
     // only run ESLint during `next dev`, skip it on `next build`
